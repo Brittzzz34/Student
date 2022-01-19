bdadbdeaaa8c9710e35cfc94f1787bad9066f35e
Brittzzz34 <hanford@sheridancollege.ca>
2022-01-19 09:45:11 -0500

    First Commit

---
+++ src/student/Student.java
@@ -0,0 +1,50 @@
+
+package student;
+
+import java.util.Scanner;
+
+
+public class Student {
+
+    public static void main(String[] args) {
+/*
+        char[] myWord = new char[7];
+        myWord[0] = 's';
+        myWord[1] = 't';
+        myWord[2] = 'u';
+        myWord[3] = 'd';
+        myWord[4] = 'e';
+        myWord[5] = 'n';
+        myWord[6] = 't';
+        
+        System.out.println("myWord:"+myWord.length);        //prints out length of array
+        
+        for (int i = 0; i<myWord.length; i++) {             //prints out the values listed in the array
+            System.out.println(myWord[i]);
+        }
+        
+        System.out.println("Printing in reverse");
+        
+        
+        for (int i = myWord.length-1;i>=0; i--) {           //prints out the values listed in the array IN REVERSE
+            System.out.println(myWord[i]);
+        }
+*/
+        
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a word:");
+        String word = input.nextLine();
+        
+        
+        char[] myLetters = new char[word.length()] ;  
+    
+        for (int i = 0; i<myLetters.length; i++) {             
+            myLetters[i]=word.charAt(i);                      //adds the characters to the array from the word
+            
+            System.out.println(myLetters[i]);
+        }
+    
+    }
+    
+}
---
+++ src/student/StudentList.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author Brian
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StudentID[] studentList = new StudentID[3];
+        studentList[0] = new StudentID("s1", "Brittany");
+        studentList[1] = new StudentID("s2", "Brooke");
+        studentList[2] = new StudentID("s3", "Brian");
+    
+    
+    for (int i = 0; i<studentList.length; i++){
+            System.out.println(studentList[i].getStudentName());
+            System.out.println(studentList[i].getStudentID());
+    }
+    
+//    for(String student : studentList){
+//        System.out.println("");
+//    }
+}
+}
